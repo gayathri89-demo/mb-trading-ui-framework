002b0578b785756b18fc619e5d28642c5e6cc056
Gayathri <gayathri.r.nair89@gmail.com>
2026-04-04 23:06:26 +0400

    feat: Add page ojects for Homepage

---
+++ cypress/pageObjects/HomePage.js
@@ -0,0 +1,17 @@
+import BasePage from "./BasePage";
+
+class HomePage extends BasePage {
+  open() {
+    this.visit("https://mb.io/");
+  }
+
+  verifyHero(texts) {
+    texts.forEach((text) => this.verifyText(text));
+  }
+
+  verifyWhySection() {
+    this.scrollToText("Why MultiBank Group?");
+  }
+}
+
+export default new HomePage();
